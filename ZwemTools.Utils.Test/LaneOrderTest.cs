a0a04584099f501ef7f27f7de7d00f141ee02cc5
Sven Dubbeld <sven@svendubbeld.nl>
2025-09-09 18:58:41 +0200

    Use correct lane order in finals

---
+++ ZwemTools.Utils.Test/LaneOrderTest.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="LaneOrderTest.cs" company="ZwemTools">
+// Copyright (c) ZwemTools. All rights reserved.
+// </copyright>
+
+namespace ZwemTools.Utils.Test;
+
+[TestFixture]
+[TestOf(typeof(LaneOrder))]
+public class LaneOrderTest
+{
+    [Test]
+    [TestCase(1, 1, new[] { 1 })]
+    [TestCase(1, 2, new[] { 1, 2 })]
+    [TestCase(1, 3, new[] { 2, 1, 3 })]
+    [TestCase(1, 4, new[] { 2, 3, 1, 4 })]
+    [TestCase(1, 5, new[] { 3, 2, 4, 1, 5 })]
+    [TestCase(1, 6, new[] { 3, 4, 2, 5, 1, 6 })]
+    [TestCase(1, 7, new[] { 4, 3, 5, 2, 6, 1, 7 })]
+    [TestCase(1, 8, new[] { 4, 5, 3, 6, 2, 7, 1, 8 })]
+    [TestCase(2, 7, new[] { 4, 5, 3, 6, 2, 7 })]
+    public void TestGetLaneOrder(int startLane, int endLane, int[] expectedLaneOrder)
+    {
+        IEnumerable<int> laneOrder = LaneOrder.GetLaneOrder(startLane, endLane);
+        Assert.That(laneOrder, Is.EquivalentTo(expectedLaneOrder));
+    }
+}
---
+++ ZwemTools.Utils/LaneOrder.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="LaneOrder.cs" company="ZwemTools">
+// Copyright (c) ZwemTools. All rights reserved.
+// </copyright>
+
+namespace ZwemTools.Utils;
+
+/// <summary>
+/// Lane order utilities.
+/// </summary>
+public static class LaneOrder
+{
+    /// <summary>
+    /// Returns the order of the lanes use for assigning athletes to heats.
+    /// </summary>
+    /// <param name="startLane">The leftmost lane.</param>
+    /// <param name="endLane">The rightmost lane.</param>
+    /// <returns>The order of the lanes.</returns>
+    public static IEnumerable<int> GetLaneOrder(int startLane, int endLane)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(endLane, startLane);
+
+        int count = endLane - startLane + 1;
+        int firstLane = (int)Math.Floor((startLane + endLane) / 2d);
+        int currentLane = firstLane;
+
+        int nextSign = count % 2 == 0 ? 1 : -1;
+
+        for (int i = 1; i <= count; i++)
+        {
+            yield return currentLane;
+
+            currentLane += nextSign * i;
+            nextSign *= -1;
+        }
+    }
+}
